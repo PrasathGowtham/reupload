18ffb1575097036987d1cf74ed45fb6da83cf0db
PrasathGowtham <108874931+PrasathGowtham@users.noreply.github.com>
2022-12-15 16:43:51 +0530

    Add files via upload

---
+++ src/components/Hero.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from 'react';
+import Lottie from 'lottie-web';
+import animate from 'lottie-web';
+
+const Hero = () => {
+  const container = useRef(null);
+
+  useEffect(() => {
+    Lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('./astronaout.json'),
+    });
+    animate.setSpeed(0.5);
+  }, []);
+
+  return (
+    <section className='my-16 px-5 md:my-32 flex flex-col flex-wrap space-y-16 md:space-y-0 patterns'>
+      <div className='ml-auto w-72 md:w-96 lg:w-2/5'>
+        <div className='container' ref={container}></div>
+      </div>
+      <div className='max-w-md mb-auto space-y-5'>
+        <h1 className='text-5xl font-bold md:text-7xl'>Hello, I am Gowtham Prasath</h1>
+        <p className='tracking-wide leading-relaxed'>
+        About a years ago I decided I wanted more from my career - plus I really love learning, So I decided to teach myself how to code and I started with HTML5 and CSS3 for Better knowladge I moved to Full Stack devolepment Course (MERN) in Guvi Geek Institue from june 2022 to oct 2022 before my Full Stack Course I had complited B.E Civil Engineering in year 2018, and  worked as site Engineer in Apexinfralink limited till May 2022.      </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/projectData.js
@@ -0,0 +1,44 @@
+
+const projects = [ {
+  title: 'FOOD TOKEN GENERATOR',
+  image: './images/Untitled',
+  description: ' Food Token Generator for inhouse app in which user can select a fixed food menu and generate a token for the food order ',      
+  tools: ['React','Node Js', 'bcrypt', 'MongoDB', 'Express','JWT' ,'Express','Material UI'],
+  github: 'https://github.com/PrasathGowtham/FoodTokenServer/tree/main',
+  link: 'https://warm-clafoutis-f07e0d.netlify.app/',
+},
+    {
+      title: 'MOVIEE TICKET BOOKING APP',
+      image: './images/ticket',
+      description: ' A Ticket booking app is a model of Moviee ticket booking app ',      
+      tools: ['React','Node Js', 'bcrypt', 'MongoDB', 'Express','JWT' ,'Express'],
+      github: 'https://github.com/PrasathGowtham',
+      link: 'https://tranquil-starship-e35c05.netlify.app/#s',
+    },
+    {
+      title: 'DASH BOARD CLONE',
+      image: './images/dashbord',
+      description: 'This is a basic front end Dash board clone Application.',
+      tools: ['React','HTML','Css','Bootstarp'],
+      github: 'https://github.com/PrasathGowtham',
+      link: 'https://bejewelled-shortbread-4e8972.netlify.app/',
+    },
+    {
+      title: 'E-COMMERSE SITE',
+      image: './images/ecomm',
+      description: 'This E-commerse site is a basic front end React app Clone',
+      tools: ['React','HTML','Css','Bootstarp'],
+      github: 'https://github.com/PrasathGowtham',
+      link: 'https://rad-churros-eac9aa.netlify.app/?#',
+    },
+    {
+      title: 'Nationalize API',
+      image: './images/nation',
+      description: 'App is based on	By Searching the Name of Person, We get the response back by displaying the top two countries of Name and it also display the probability value..',
+      tools: ['HTML','Css','Javascript'],
+      github: 'https://github.com/PrasathGowtham',
+      link: 'https://gilded-sprinkles-278914.netlify.app',
+    },
+  ];
+  
+  export default projects;
